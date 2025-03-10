ee0eb10500f1145b10cd30344a232e8e277db2c7
EngBasma2030 <basmabahaa610@gmail.com>
2025-03-11 00:49:07 +0200

    وصف التغييرات

--- Entity Framework Core01/Program.cs
+++ Entity Framework Core01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Entity_Framework_Core01.Contexts;
+using Entity_Framework_Core01.Entites;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace Entity_Framework_Core01
 {
@@ -6,7 +9,79 @@ namespace Entity_Framework_Core01
     {
         static void Main(string[] args)
         {
-            ITIDbContext dbContext = new ITIDbContext();
+            using ITIDbContext dbContext = new ITIDbContext();
+
+            dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.TrackAll;
+
+            Student stud01 = new Student()
+            {
+                FName = "Basma",
+                LName = "Bahaa",
+                Address = "Giza",
+                Age = 22
+            };
+            Student stud02 = new Student()
+            {
+                FName = "Aya",
+                LName = "Bahaa",
+                Address = "Giza",
+                Age = 25
+            };
+
+            #region Insert
+            //Console.WriteLine(dbContext.Entry(stud01).State);
+
+            //dbContext.Students.Add(stud01); 
+            //dbContext.Set<Student>().Add(stud01);
+            //dbContext.Add(stud01); 
+            //dbContext.Entry(stud01).State = EntityState.Added;
+
+            //dbContext.Add(stud02);
+            //Console.WriteLine("After Adding");
+            //Console.WriteLine(dbContext.Entry(stud01).State); 
+
+            //dbContext.SaveChanges();
+            //Console.WriteLine("After save Changes");
+            //Console.WriteLine(dbContext.Entry(stud01).State); 
+            #endregion
+
+            var student = dbContext.Students.SingleOrDefault(S =>S.Id  == 3);
+            var student02 = dbContext.Students.AsNoTracking().SingleOrDefault(S => S.Id  == 4);
+
+            #region Update
+            //if (student != null)
+            //{
+            //    student.FName = "Noha";
+            //}
+
+            //if (student02 != null)
+            //{
+            //    student02.FName = "Mona";
+            //}
+            //dbContext.SaveChanges();
+
+            //Console.WriteLine(dbContext.Entry(student).State);
+            //Console.WriteLine(dbContext.Entry(student02).State);
+            #endregion
+
+            if (student != null)
+            {
+                Console.WriteLine(dbContext.Entry(student).State); 
+
+                dbContext.Students.Remove(student);
+                dbContext.Set<Student>().Remove(student);
+                dbContext.Remove(student);
+                dbContext.Entry(student).State = EntityState.Deleted;
+
+                Console.WriteLine("After delete");
+                Console.WriteLine(dbContext.Entry(student).State); 
+
+                Console.WriteLine("After savechanges");
+                dbContext.SaveChanges();
+                Console.WriteLine(dbContext.Entry(student).State);
+
+                Console.WriteLine(stud01);
+            }
         }
     }
 }
